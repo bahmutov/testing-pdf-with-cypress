be5f4c970e0814225f05bf3b31acb4c7bec64361
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-08-10 16:23:53 -0400

    format readpdf

--- cypress/scripts/readPdf.ts
+++ cypress/scripts/readPdf.ts
@@ -1,19 +1,13 @@
-const fs = require("fs");
+const fs = require('fs')
 const path = require('path')
-const pdf = require('pdf-parse');
+const pdf = require('pdf-parse')
 
 export const readPdf = (pathToPdf: string) => {
-
-    return new Promise((resolve) => {
-        const pdfPath = path.resolve(pathToPdf)
-        let dataBuffer = fs.readFileSync(pdfPath);
-        pdf(dataBuffer).then(function ({ text }) {
-
-            resolve(text)
-
-        });
+  return new Promise((resolve) => {
+    const pdfPath = path.resolve(pathToPdf)
+    let dataBuffer = fs.readFileSync(pdfPath)
+    pdf(dataBuffer).then(function ({ text }) {
+      resolve(text)
     })
-
-
-
+  })
 }
